9a9cc01bc8e132cf3a2f243d061a6844e7dba5e7
aparson1982 <aparson1982@gmail.com>
2019-11-21 12:38:18 -0500

    Add files via upload

---
+++ Selenium Tutorial/Selenium Tutorial/Program.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Tutorial
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            PropertiesCollection.driver = new ChromeDriver();
+            PropertiesCollection.driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            Console.WriteLine("Initializing...");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            LoginPageObject login = new LoginPageObject();
+            EAPageObjects page = login.Login("execute", "automation");
+            _ = PropertiesCollection.driver.Manage().Timeouts().ImplicitWait;
+            //new WebDriverWait(PropertiesCollection.driver, TimeSpan.FromSeconds(20)).Until(
+            //        d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+            page.FillUserForm("KK", "Karthik", "Automation");
+
+
+
+
+
+
+
+
+
+
+
+            //SeleniumSetMethod.SelectDropDown("TitleId", "Ms.", PropertyType.Id);
+
+            //SeleniumSetMethod.EnterText("Initial", "Testing", PropertyType.Name);
+            //SeleniumSetMethod.EnterText("FirstName", "Jesus", PropertyType.Id);
+            //SeleniumSetMethod.EnterText("MiddleName", "Carrillo", PropertyType.Id);
+
+            //Console.WriteLine("The value from my Title is:  " + SeleniumGetMethods.GetTextFromDDL("TitleId", PropertyType.Id));
+            //Console.WriteLine("The value from my Initial is:  " + SeleniumGetMethods.GetText("Initial", PropertyType.Name));
+
+            ////Console.WriteLine("The value from Title using GetTextDDL is:  " + SeleniumGetMethods.GetTextFromDDL(webDriver, "TitleId", "Id");
+
+            //SeleniumSetMethod.Click("Save", PropertyType.Name);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            PropertiesCollection.driver.Close();
+        }
+    }
+}
---
+++ Selenium Tutorial/Selenium Tutorial/SeleniumSetMethod.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Tutorial
+{
+    class SeleniumSetMethod
+    {
+        /// <summary>
+        /// Enter Text
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="element"></param>
+        /// <param name="value"></param>
+        /// <param name="elementType"></param>
+        public static void EnterText(IWebElement element, string value)
+        {
+
+            element.SendKeys(value);
+            
+        }
+
+
+
+        public static void Click(IWebElement element)
+        {
+            element.Click();
+        }
+
+
+
+        public static void SelectDropDown(IWebElement element, string value)
+        {
+            new SelectElement(element).SelectByText(value);
+        }
+    }
+}
